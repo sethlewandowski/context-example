1a08932f3ef5e4a0c64bdcb2b655e5dd4f9da298
seth <sethlew@protonmail.com>
2023-02-08 13:13:52 -0600

    Fixed by changing the destructuring from useContext.

--- src/Page2.js
+++ src/Page2.js
@@ -4,11 +4,11 @@ import { SharedContext } from './context';
 
 export default function Page2() {
     const navigate = useNavigate();
-    const { context } = useContext(SharedContext);
-    const [num, setNum] = context;
+    const { context, setContext } = useContext(SharedContext);
+    // const [num, setNum] = context;
 
     const onClick = () => {
-        setNum(1);
+        setContext(1);
         navigate('/page3');
     };
 
